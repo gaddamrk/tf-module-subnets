1973e5b7e6d9b57df518dae0112de4375e771483
gaddamrk <raj>
2023-08-16 17:04:09 +0530

    jr

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,12 +1,33 @@
-module "subnets" {
-  source = "./subnets"
-  availability_zone = var.availability_zone
-  cidr_block = var.cidr_block
-  default_vpc_id = var.defadefault_vpc_id
-  env = var.env
-  name = var.name
+resource "aws_subnet" "main" {
+  count = length(var.cidr_block)
+  cidr_block = var.cidr_block[count.index]
   vpc_id = var.vpc_id
-  vpc_peering_connection_id = var.vpc_peering_connection_id
+  tags       = merge(
+    local.common_tags,
+    { Name = "${var.env}-${var.name}-subnet-${count.index+1}" }
+  )
+
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block                = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = var.vpc_peering_connection_id
+  }
+
+  tags       = merge(
+    local.common_tags,
+    { Name = "${var.env}-${var.name}-route_table" }
+  )
+
+}
+
+resource "aws_route_table_association" "association" {
+  count = length(aws_subnet.main)
+  subnet_id      = aws_subnet.main.*.id[count.index]
+  route_table_id = aws_route_table.route_table.id
 }
 
 
@@ -45,6 +66,7 @@ module "subnets" {
 #  )
 #
 #}
+
 // where we wanted created all the time
 
 // if i have a separate natgateways for like i was taking about other day we generally go with the multiple natgateways
--- vars.tf
+++ vars.tf
@@ -1,10 +1,5 @@
-variable "cidr_block" {}
-variable "availability_zone" {}
-variable "vpc_id" {}
-variable "env" {}
-variable "default_vpc_id" {}
-variable "name" {}
-variable "vpc_peering_connection_id" {}
+
+
 variable "internet_gw" {}
 variable "nat_gw" {}
-variable "internet_gw_id" {}
+#variable "internet_gw_id" {} here we are removed because it creating here only
